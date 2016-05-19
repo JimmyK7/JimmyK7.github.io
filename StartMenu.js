3575701fa29e4e8de4b1b1529754a23f98301476
Ron Swanson <adeegan9@gmail.com>
2016-05-19 09:09:54 -0400

    I made the StartMenu

---
+++ StartMenu.js
@@ -0,0 +1,30 @@
+TrumpJump.StartMenu = function(game) {
+    this.startBG;
+    this.startPrompt;
+    this.ding
+}
+
+TrumpJump.Startmenu.prototype = 
+	
+create; function () { 
+	this.ding = this.add.audio('select_audio')
+	
+	startBG = this.add.image(0, 0, 'titlescreen);
+	startBG.inputEnabled = true;
+	startBG.events.onInputDown.addOnce(this.startgame, this);
+	
+	
+	startPrompt = this.add.bitmapText(this.world.centerX-155, this.world.centerY+100, 'eightbitwonder', 'Touch to Start!', 24);
+	
+	
+}, 
+	startGame: function (pointer) {
+	this.ding.play();
+	this.state.start('Game');
+  }
+	
+}; 
+	
+
+
+
